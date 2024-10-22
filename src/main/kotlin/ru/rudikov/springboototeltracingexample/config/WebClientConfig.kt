593a2ec0a1ba09817235f0ebd7fc203a8f661a00
e.rudikov <e.rudikov@tinkoff.ru>
2024-10-22 17:13:02 +0300

    feat: add opentelemetry tracing

--- src/main/kotlin/ru/rudikov/springboototeltracingexample/config/WebClientConfig.kt
+++ src/main/kotlin/ru/rudikov/springboototeltracingexample/config/WebClientConfig.kt
@@ -9,6 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 class WebClientConfig {
 
+    // If you create the RestTemplate, the RestClient or the WebClient without using the auto-configured builders, automatic trace propagation won’t work!
+    // https://docs.spring.io/spring-boot/reference/actuator/tracing.html#actuator.micrometer-tracing.propagating-traces
     @Bean
     fun webClientBuilder(): WebClient.Builder = WebClient.builder()
 
